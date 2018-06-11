ac38eb7298ed2762fd4c4e07e446b718a8bb51be
Qim Yi Lee <qimyi.lee@gmail.com>
2018-06-12 09:18:57 +1000

    AOP2-44: Give mock arguments names

--- frontend/apps/orams/actions/referralActions.test.js
+++ frontend/apps/orams/actions/referralActions.test.js
@@ -25,41 +25,54 @@ describe('referralActions', () => {
 
     dmapi.mockReturnValueOnce(Promise.resolve(responseData))
     loadReferralData(321)(mockDispatch)
-    expect(getMockCallArg(dmapi, 0, 0)).toEqual({ url: 'referrals/321' })
+    const apiClientCallArg = getMockCallArg(dmapi, 0, 0)
+    expect(apiClientCallArg).toEqual({ url: 'referrals/321' })
   })
 
   it('should set referral data to state from response', () => {
     const responseData = { data: { price: 200 } }
-    const expectedAction = { type: SET_REFERRAL_DATA, referralData: { price: 200 } }
+    const setReferralAction = { type: SET_REFERRAL_DATA, referralData: { price: 200 } }
     dmapi.mockReturnValueOnce(Promise.resolve(responseData))
 
     return loadReferralData(1)(mockDispatch).then(() => {
-      expect(getMockCallArg(mockDispatch, 0, 0)).toEqual(setLoadingAction)
-      expect(getMockCallArg(mockDispatch, 1, 0)).toEqual(resetLoadingAction)
-      expect(getMockCallArg(mockDispatch, 2, 0)).toEqual(expectedAction)
+      const firstCallFirstArg = getMockCallArg(mockDispatch, 0, 0)
+      const secondCallFirstArg = getMockCallArg(mockDispatch, 1, 0)
+      const thirdCallFirstArg = getMockCallArg(mockDispatch, 2, 0)
+
+      expect(firstCallFirstArg).toEqual(setLoadingAction)
+      expect(secondCallFirstArg).toEqual(resetLoadingAction)
+      expect(thirdCallFirstArg).toEqual(setReferralAction)
     })
   })
 
   it('should set general error message', () => {
     const responseData = { error: { message: 'some error' } }
-    const expectedAction = { type: SET_ERROR_MESSAGE, errorMessage: GENERAL_ERROR }
+    const expectedErrorAction = { type: SET_ERROR_MESSAGE, errorMessage: GENERAL_ERROR }
     dmapi.mockReturnValueOnce(Promise.resolve(responseData))
 
     return loadReferralData(1)(mockDispatch).then(() => {
-      expect(getMockCallArg(mockDispatch, 0, 0)).toEqual(setLoadingAction)
-      expect(getMockCallArg(mockDispatch, 1, 0)).toEqual(resetLoadingAction)
-      expect(getMockCallArg(mockDispatch, 2, 0)).toEqual(expectedAction)
+      const firstCallFirstArg = getMockCallArg(mockDispatch, 0, 0)
+      const secondCallFirstArg = getMockCallArg(mockDispatch, 1, 0)
+      const thirdCallFirstArg = getMockCallArg(mockDispatch, 2, 0)
+
+      expect(firstCallFirstArg).toEqual(setLoadingAction)
+      expect(secondCallFirstArg).toEqual(resetLoadingAction)
+      expect(thirdCallFirstArg).toEqual(expectedErrorAction)
     })
   })
 
   it('should set unauthorised error message', () => {
     const responseData = { error: 'some error', status: 403 }
-    const expectedAction = { type: SET_ERROR_MESSAGE, errorMessage: UNAUTHORISED_ERROR }
+    const expectedErrorAction = { type: SET_ERROR_MESSAGE, errorMessage: UNAUTHORISED_ERROR }
     dmapi.mockReturnValueOnce(Promise.resolve(responseData))
     return loadReferralData(1)(mockDispatch).then(() => {
-      expect(getMockCallArg(mockDispatch, 0, 0)).toEqual(setLoadingAction)
-      expect(getMockCallArg(mockDispatch, 1, 0)).toEqual(resetLoadingAction)
-      expect(getMockCallArg(mockDispatch, 2, 0)).toEqual(expectedAction)
+      const firstCallFirstArg = getMockCallArg(mockDispatch, 0, 0)
+      const secondCallFirstArg = getMockCallArg(mockDispatch, 1, 0)
+      const thirdCallFirstArg = getMockCallArg(mockDispatch, 2, 0)
+
+      expect(firstCallFirstArg).toEqual(setLoadingAction)
+      expect(secondCallFirstArg).toEqual(resetLoadingAction)
+      expect(thirdCallFirstArg).toEqual(expectedErrorAction)
     })
   })
 })
